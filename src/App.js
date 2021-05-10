06a4e81643e05da056db540a0d42bd69fbd1ebe9
Mykola Prymak <mykola.prymak@gmail.com>
2021-05-11 00:22:03 +0200

    feat: add lern more link to the page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <h1>
+          Learn Roman together
+        </h1>
+        <input type="text" onChange={() => {}} />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://en.wikipedia.org/wiki/Roman_numerals"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more on Wikipedia (English)
         </a>
       </header>
     </div>
