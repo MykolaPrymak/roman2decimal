f38e448c89c2b7aebb21230f1fcc919bf8af53c0
Mykola Prymak <mykola.prymak@gmail.com>
2021-05-11 22:08:58 +0200

    feat: optimize and finish the decimal to roman converter helper

--- src/helpers/RomanNumerals.ts
+++ src/helpers/RomanNumerals.ts
@@ -7,7 +7,6 @@ export const ROMAN_FIVE_HUNDRED = "D";
 export const ROMAN_THOUSAND = "M";
 export const ROMAN_MAX = 3999;
 
-
 const RomanNumerals = {
     toRoman: (decimal: number):string => {
         if (decimal > ROMAN_MAX) {
@@ -16,38 +15,34 @@ const RomanNumerals = {
 
         const romans = [];
         let position = 0;
+        // We will use this pairs of letters to create the number that we want
+        const positionLetters = [
+            // [lessSignificantLetter, moreSignificantLetter]
+            [ROMAN_ONE, ROMAN_FIVE],
+            [ROMAN_TEEN, ROMAN_FIFTY],
+            [ROMAN_HUNDRED, ROMAN_FIVE_HUNDRED],
+            [ROMAN_THOUSAND],
+        ]
 
-        //console.log('decimal', decimal)
         while (position < 4) {
             const base = Math.pow(10, position);
             const positionValue = (decimal % (base * 10)) / base;
 
-            //console.log({positionValue, position, base});
-            switch (position) {
-                case 0:
-                    if (positionValue < 4) {
-                        romans.unshift(ROMAN_ONE.repeat(positionValue));
-                    } else if (positionValue >= 5 && positionValue < 9) {
-                        romans.unshift(`${ROMAN_FIVE}${ROMAN_ONE.repeat(positionValue - 5)}`);
-                    } else {
-                        // For 4 and 9 we should avoid adding four IIII
-                        romans.unshift(`${ROMAN_ONE}${positionValue === 4 ? ROMAN_FIVE : ROMAN_TEEN}`);
-                    }
-                break;
-                case 1:
-                    if (positionValue < 4) {
-                        romans.unshift(ROMAN_TEEN.repeat(positionValue));
-                    } else if (positionValue >= 5 && positionValue < 9) {
-                        romans.unshift(`${ROMAN_FIFTY}${ROMAN_TEEN.repeat(positionValue - 5)}`);
-                    } else {
-                        // For 4 and 9 we should avoid adding four IIII
-                        romans.unshift(`${ROMAN_TEEN}${positionValue === 4 ? ROMAN_FIFTY : ROMAN_HUNDRED}`);
-                    }
-                break;
+            const [lessSignificantLetter, moreSignificantLetter] = positionLetters[position];
+            const [nextPositionLetter] = positionLetters[position + 1] || []; // The or is for the last position
+
+            if (positionValue < 4) {
+                romans.unshift(lessSignificantLetter.repeat(positionValue));
+            } else if (positionValue >= 5 && positionValue < 9) {
+                romans.unshift(`${moreSignificantLetter}${lessSignificantLetter.repeat(positionValue - 5)}`);
+            } else {
+                // For 4 and 9 we should avoid adding four the same letters
+                romans.unshift(`${lessSignificantLetter}${positionValue === 4 ? moreSignificantLetter : nextPositionLetter}`);
             }
 
             // Remove the value that we process
             decimal -= (positionValue * base);
+
             // Go to next step
             position++;
         }
